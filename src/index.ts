cf24757acf17d376984327701b9a6010ecd0acd4
Nepalcem <mark.emailtest@gmail.com>
2024-12-04 21:27:38 +0200

    wss add

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { WebSocketServer } from "ws";
+import { createServer } from "http";
 
 dotenv.config({ path: path.join(__dirname, "environment", ".env") });
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+
+
+wss.on("connection", (ws) => {
+  console.log("A client connected");
+
+  ws.on("message", (message) => {
+    console.log(`Received: ${message}`);
   });
-  
-  app.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+
+  ws.on("close", () => {
+    console.log("A client disconnected");
   });
+
+  ws.send("Welcome to the WebSocket chat!");
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server D is running at http://localhost:${PORT}`);
+});
